e572ff279d2b6e65ab50b13eab59c451964d69c4
fpspeixoto <fps.engenheiro@gmail.com>
2022-09-24 19:15:55 -0300

    inicio

---
+++ src/main/java/com/autoresidencia/autoresidencia/AutoresidenciaApplication.java
@@ -0,0 +1,13 @@
+package com.autoresidencia.autoresidencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoresidenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoresidenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoresidencia/autoresidencia/AutoresidenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoresidencia.autoresidencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoresidenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
