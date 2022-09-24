889ea0049dc5d0c8be1ba8e1a56e36998bc64050
fpspeixoto <fps.engenheiro@gmail.com>
2022-09-24 20:38:38 -0300

    inicio

---
+++ src/main/java/com/autoresidencia/autoresidencia/controller/CaixaDaguaController.java
@@ -0,0 +1,15 @@
+package com.autoresidencia.autoresidencia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/caixadagua")
+public class CaixaDaguaController {
+
+    @GetMapping("/start")
+    public boolean setStartCaixaDagua(){
+        return true;
+    }
+}
